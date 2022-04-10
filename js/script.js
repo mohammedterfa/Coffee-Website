326cd181ac36bc2ca5b2deb60d801f099bcb199c
mohammedterfa <mohammedterfa@gmail.com>
2022-04-10 22:02:20 +0200

    search btn now working

--- js/script.js
+++ js/script.js
@@ -4,6 +4,11 @@ document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
 }
 
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+}
 
 let cartItem = document.querySelector('.cart-items-container');
 
